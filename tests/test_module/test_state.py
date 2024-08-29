876fa17598bafb51797835e28b50c1fff1af8bf4
ruu23 <arwamah777@gmail.com>
2024-08-29 04:18:07 +0300

    state

--- tests/test_module/test_state.py
+++ tests/test_module/test_state.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-import unittest
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
-class TestState(unittest.TestCase):
 
-    def test_attributes(self):
-        state = State()
-        self.assertEqual(state.name, "")
+class test_state(test_basemodel):
+    """ """
 
-if __name__ == '__main__':
-    unittest.main()
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
+
+    def test_name3(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
